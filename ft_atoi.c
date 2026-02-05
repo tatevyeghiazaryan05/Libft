ec061e4ba80b76ce88e4fa5a5902a02eb5fd7382
root <root@DESKTOP-63BM6S7.>
2026-02-05 11:14:58 +0400

    Atoi function implementation

---
+++ ft_atoi.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+int	ft_atoi(conts char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	* ft_strnstr (const char *big, const char *little, size_t len)
+{
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+
+	if (!little)
+		return ((char *)big);
+	while(i < len && big[i] != '\0')
+	{
+		j = 0;
+		while(little[j] != '\0' &&)
+	}
+}
